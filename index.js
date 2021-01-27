37a0bef28e05610860349d1a39f55588f6aa014c
Bob <rstauss@burlingtoncodeacademy.com>
2021-01-27 16:34:12 -0500

    Unnecessary comment

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
+  
   process.exit();
 }
